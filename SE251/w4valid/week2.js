a592e4db09c0e42a9b64e38e7f23591fb40eab68
Sebastian Oquendo <soquendo@email.neit.edu>
2022-09-20 04:39:21 -0400

    changes

--- SE251/w4valid/week2.js
+++ SE251/w4valid/week2.js
@@ -1,25 +1,23 @@
 // JavaScript Document
 
 
-var fName = document.querySelector(`#first-name`)
-var lName = document.querySelector(`#last-name`)
-var email = document.querySelector(`#email`)
-var emailCon = document.querySelector(`#emailConfirm`)
-var phone = document.querySelector(`#phone`)
-var form = document.querySelector(`#form`)
-var conf = document.querySelector(`#confirmation`)
-var bool = true
+
+
 
 function e()
 {
-    
+    var fName = document.querySelector(`#first-name`)
+    var lName = document.querySelector(`#last-name`)
+    var email = document.querySelector(`#email`)
+    var emailCon = document.querySelector(`#emailConfirm`)
+    var phone = document.querySelector(`#phone`)
     var inputs = document.querySelectorAll(`input[type="text"]`)
     var p = document.querySelectorAll(`p`)
-
-    var button = document.querySelector(`input[type="button"]`)
-    var output = document.querySelector(`#output`)
-    
     var spans =  document.querySelectorAll(`span`)
+    //var button = document.querySelector(`input[type="button"]`)
+    //var output = document.querySelector(`#output`)
+    var bool = true
+    
     
     
     for(let i=0; i<inputs.length;i++)
@@ -33,11 +31,11 @@ function e()
         }
         else if (inputs[2].value !== inputs[3].value)
         {
-            p[2].style.color=`red`
+            p[2].style.color="red"
             spans[2].innerText = `*`
             spans[2].style.display = `inline`
 
-            p[3].style.color= `red`
+            p[3].style.color= "red"
             spans[3].innerText = `*`
             spans[3].style.display = `inline`
             bool = false
@@ -48,22 +46,24 @@ function e()
             spans[i].innerText = ""
             spans[i].style.display = `inline` 
         }
+
         if (bool == true)
         {
             document.querySelector(`#form`).style.display = `none`
             document.querySelector(`#confirmation`).style.display = `block`
-            
             document.querySelector(`#info`).innerText = `${inputs[0].value} ${inputs[1].value}\n
             ${inputs[2].value}\n
             ${inputs[4].value}`
         }
+        
         else
         {
-            document.querySelector(`#confirmation`).style.display = `none`
             document.querySelector(`#form`).style.display = `block`
+            document.querySelector(`#confirmation`).style.display = `none`
+            
         }
 
-        console.log(inputs[0].value)
+        //console.log(inputs[0].value)
 
 
     }
